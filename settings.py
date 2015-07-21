bd459336d881dda858a5feb9cdf7b4b5a0ae5174
asuciu <adisuciu88@yahoo.com>
2015-07-21 20:12:29 +0300

    Minor tweaks:
    updatefrequency increasec to 1.5 seconds
    logfile now contains date and time of the beginning of the session

--- settings.py
+++ settings.py
@@ -5,11 +5,11 @@ imgur_api_client_id = "afc6b8634860754"
 imgflip_username = "AeBBot"
 imgflip_password = "aebbotpassword"
 dt = datetime.datetime.now()
-updateFrequency = 1  # seconds
+updateFrequency = 1.5  # seconds
 quote_file = "bug_mafia.txt"
 links_file = "links.txt"
-# log_file = "log-%d%02d%02d-%02d%02d%02d.txt" % (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
-log_file = "log.txt"
+log_file = "log-%d%02d%02d-%02d%02d%02d.txt" % (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+# log_file = "log.txt"
 stats_file = "stats.txt"
 about_message = "Aici este BAIETII Official bot - v0.1\nSources available at: " \
                 "https://github.com/adisuciu/AeB-Telegram-Bot\n"
