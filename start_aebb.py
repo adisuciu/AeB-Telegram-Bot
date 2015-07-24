be38f72e831318b550110d002fc22e80ad024713
asuciu <adisuciu88@yahoo.com>
2015-07-24 15:38:22 +0300

    Fixed autorestart app

--- start_aebb.py
+++ start_aebb.py
@@ -1,17 +1,21 @@
-
 import time
 import settings
 import os
 
-with open("sync") as f:
-    t = f.read()
-sync = int(t)
+try:
+    with open("sync") as f:
+        t = f.read()
+    sync = int(t)
+except:
+    print ("file sync not found")
+    sync = 0
+
 now = time.time()
 if (now - sync) >= settings.stats_save_freq*2:
-    os.chdir("AeB-Telegram-Bot")
-    os.system("python3 aebb.py")
     with open("sync",'w') as f:
-        f.write(int(time.time()))
+        f.write(str(int(time.time())))
+    import aebb
+
 else:
     # nothing to do, app is already running
     print("App is already running")
